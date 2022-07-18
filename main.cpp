692cd07cb6934a0ee5ed712d878e4ebf86dd30a4
Benja-99 <benjamin.pizarro@sansano.usm.cl>
2022-07-18 00:41:32 -0400

    Cambios

--- main.cpp
+++ main.cpp
@@ -45,11 +45,9 @@ double haversine(double lat1, double lon1, double lat2, double lon2)
     double dLat = (lat2 - lat1) * pi / 180.0;
     double dLon = (lon2 - lon1) * pi / 180.0;
 
-    // convert to radians
     lat1 = (lat1)*pi / 180.0;
     lat2 = (lat2)*pi / 180.0;
 
-    // apply formulae
     double a = pow(sin(dLat / 2), 2) + pow(sin(dLon / 2), 2) * cos(lat1) * cos(lat2);
     double rad = 6371;
     double c = 2 * asin(sqrt(a));
@@ -175,7 +173,6 @@ string PrintVec(const vector<node> &vec)
 {
     string to_return = "", aux, distancia_string, aux_2;
     double distancia;
-    // for (size_t i = 0; i < vec.size() - 1; i++)
     for (size_t i = 0; i < vec.size(); i++)
     {
         aux = to_string(vec[i].id_interno);
@@ -183,7 +180,6 @@ string PrintVec(const vector<node> &vec)
         distancia = haversine(vec[i].latitude, vec[i].longitude, vec[i + 1].latitude, vec[i + 1].longitude);
         distancia_string = to_string(distancia);
         to_return += (aux + vec[i].tipo + " - ");
-        // to_return += (aux + "|" + aux_2 + " " + distancia_string + " - ");
     }
     return to_return;
 }
